d24eae22d2ad2bcf17e0223ef82c1a58861ad421
thealing <159199097+thealing@users.noreply.github.com>
2026-03-15 19:03:05 +0100

    Fixed prefetch

--- src/search.hpp
+++ src/search.hpp
@@ -313,7 +313,7 @@ public:
 				move_scores[i] = -1;
 				continue;
 			}
-			_mm_prefetch((const char*)&transposition_table.get_entry(_hash_stack[_ply]), _MM_HINT_T0);
+			_mm_prefetch((const char*)&transposition_table.get_entry(_hash_stack[_ply]), _MM_HINT_NTA);
 			legal_move_count++;
 			if (move.captured_piece == PIECE_NONE)
 			{
