168c5e5b58dfc9ec5ed82202abb53a360336694c
thealing <159199097+thealing@users.noreply.github.com>
2026-03-15 19:02:56 +0100

    Fixed scores

--- src/scores.hpp
+++ src/scores.hpp
@@ -4,7 +4,7 @@
 
 using Score = int32_t;
 
-inline constexpr Score SCORE_MAX = +2000000;
-inline constexpr Score SCORE_MIN = -2000000;
-inline constexpr Score SCORE_MATE = 1000000;
+inline constexpr Score SCORE_MAX = +200000;
+inline constexpr Score SCORE_MIN = -200000;
+inline constexpr Score SCORE_MATE = 100000;
 inline constexpr Score SCORE_DRAW = 0;
